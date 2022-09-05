ab3f3d66130ba17572bf2e709965624b2deb217d
varsha p <varshap630@gmail.com>
2022-09-06 03:17:40 +0530

    javascript file to delink the employee payroll

---
+++ client_server_architecture/site_properties.js
@@ -0,0 +1,6 @@
+let site_properties={
+    use_local_storage:"true",
+    home_page:"../Homepage/new_home.html",
+    add_emp_payroll_page:"../Employee_payroll/emp_payroll.html",
+    server_url:""
+};
